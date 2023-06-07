5441d3b6ecce49d07663b5acdcc16678f0f0bf58
7Cedars <Cedars7@proton.me>
2023-06-07 10:06:39 +0100

    Initial set up

---
+++ bloglist-backend/controllers/blogs.js
@@ -0,0 +1,71 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const { tokenExtractor, userExtractor } = require('../utils/middleware')
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  .populate('user', { 
+    username: 1, 
+    name: 1 }) 
+  response.json(blogs)    
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  
+  if (!blog) {response.status(404).end()}
+
+  response.json(blog)    
+})
+
+blogsRouter.post('/', tokenExtractor, userExtractor, async (request, response) => {
+  const blog = new Blog(request.body)
+  
+  blog.likes ? blog.likes = blog.likes : blog.likes = 0
+  
+  blog.user = request.user.id
+  const savedBlog = await blog.save()
+
+  request.user.blogs = request.user.blogs.concat(savedBlog.id)
+  await request.user.save()
+
+  response.status(201).json(savedBlog) 
+  
+})
+
+blogsRouter.delete('/:id', tokenExtractor, userExtractor, async (request, response) => {
+  
+  try {
+    const blog = await Blog.findById(request.params.id)
+    const userid = request.user.id
+
+    if ( blog.user.toString() === userid.toString() ){
+      await Blog.findByIdAndRemove(request.params.id)
+      response.status(204).json(request.params.id)
+    } 
+  } catch {
+    response.status(401).end()
+  }
+})
+
+// ok, so this is not the most efficient way.. but it works! 
+blogsRouter.put('/:id', tokenExtractor, userExtractor, async (request, response) => {
+  const update = request.body
+  const originalBlog = await Blog.findById(request.params.id)
+  let updatedBlog = originalBlog
+
+  if (!(originalBlog)) {response.status(404).end()}
+
+  update.title ? updatedBlog.title = update.title : null
+  update.author ? updatedBlog.author = update.author : null
+  update.url ? updatedBlog.url = update.url : null
+  update.likes ? updatedBlog.likes = update.likes : null
+
+  console.log('updatedBlog: ', updatedBlog)
+
+  savedBlog = await Blog.findByIdAndUpdate(request.params.id, updatedBlog, { new: true })
+  response.status(204).json(savedBlog)
+
+})
+
+module.exports = blogsRouter
